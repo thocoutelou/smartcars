e4339a2a8833722209ab3c9793b763106d7e3e9e
Thomas Coutelou <tcoutelou@gmail.com>
2016-02-17 11:48:56 +0100

    Première étape de la création de Graph à partir d'une image svg. Lecture d'un fichier d'exemple

--- src/tests/Test_Graph.java
+++ src/tests/Test_Graph.java
@@ -1,9 +1,17 @@
 package tests;
+import smartCars.*;
 
 public class Test_Graph {
+	
+	static String project_location="/main/work/2015-1A/projet_info/smartcars/";
+	
+	int error_number = 0;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Scenario 1 : création d'un graphe simple à partir d'une image svg
+		String fileName=project_location + "media/map/1.svg";
+		Graph graph1 = new Graph(fileName);
 
 	}
 
